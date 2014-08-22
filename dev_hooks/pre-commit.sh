90f04771384c1c7941cf041d0c81c9b5ed9b9d6f
James Tocknell <aragilar@gmail.com>
2014-08-22 11:48:13 +1000

    Added testing hooks and improved lint settings

---
+++ dev_hooks/pre-commit.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# from http://codeinthehole.com/writing/tips-for-using-a-git-pre-commit-hook/
+git stash -q --keep-index
+./dev_hooks/run_tests.sh
+RESULT=$?
+git stash pop -q
+[ $RESULT -ne 0 ] && exit 1
+exit 0
+
---
+++ dev_hooks/run_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e # exit if any of the following command fail
+
+PYTHONOPTIMIZE=1 nosetests # run the normal tests
+
+#pylint disc_solver
+#flake8 disc_solver # validate code
+
+
