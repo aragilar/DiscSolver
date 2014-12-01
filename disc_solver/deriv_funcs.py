d29a6ad1ca498b4189b3eec471b0d6898a9d73a4
James Tocknell <aragilar@gmail.com>
2014-12-01 16:58:40 +1100

    Added v_r taylor series

--- disc_solver/deriv_funcs.py
+++ disc_solver/deriv_funcs.py
@@ -3,7 +3,7 @@
 Computing derivatives
 """
 
-from math import sin, sqrt
+from math import pi, sin, sqrt
 from .utils import cot
 
 
@@ -155,4 +155,107 @@ def dderiv_B_φ_soln(
                 )
             )
         )
+    )
+
+
+def dderiv_v_r_midplane(
+    ρ, B_θ, v_r, v_φ, deriv_v_θ, deriv_B_r, deriv_B_φ, dderiv_B_θ,
+    β, η_O, η_H, η_A, c_s
+):
+    """
+    Compute v_r'' around the midplane
+    """
+    A = (1 - 2 * β) * (η_O + η_A + (η_H**2) / (η_O + η_A)) * (
+        pi * ρ * v_φ + (β-1) * B_θ * deriv_B_φ / v_r +
+        B_θ**2 * η_H / (4 * (η_H**2 + (η_O + η_A)**2))) / (
+        B_θ**2 + 4*pi * ρ * v_r * (β-1) * (2*β-1) *
+            (η_O + η_A + (η_H**2) / (η_O + η_A)))
+
+    C = (B_θ / (η_O + η_A)) * (
+        2*pi * ρ * η_H * (β-1) * (
+            2 * η_H * v_r * (2*β-1) / (η_O + η_A) + v_φ / (β-1) +
+            B_θ * deriv_B_φ / (v_r * ρ * pi)
+        ) / (
+            B_θ**2 + 4*pi * ρ * v_r * (β-1) * (2*β-1) *
+            (η_O + η_A + η_H**2/(η_O + η_A))
+        ) - 1
+    )
+
+    D = 1 + 4 * v_r * (β-1) * deriv_v_θ / c_s**2 + (
+        (β-1) * B_θ * deriv_B_r + deriv_B_r**2 + deriv_B_φ**2) / (
+            2*pi * ρ * c_s**2)
+
+    A_dash = (1 + B_θ**2 / (4*pi * ρ * v_r * (β-1) * (2*β-1) * (
+        η_O + η_A + η_H**2/(η_O + η_A))))**-1 * (
+            deriv_B_φ / (2*pi * ρ) * (
+                dderiv_B_θ - 2*(β-1) * deriv_B_r + B_θ -
+                B_θ / c_s**2 * (
+                    4 * v_r * (β-1) * deriv_v_θ + (
+                        (β-1) * B_θ * deriv_B_r + deriv_B_r**2 + deriv_B_φ**2
+                    ) / (2*pi * ρ)
+                ) + B_θ / (2*pi * ρ * (η_O + η_A + η_H**2/(η_O + η_A))) * (
+                    - 2 * dderiv_B_θ * (
+                        η_H * v_r / (η_O + η_A) + 2 * v_φ / (2*β-1)
+                    ) + deriv_B_r * (
+                        v_φ + 2 * deriv_v_θ * η_H / (η_O + η_A) +
+                        (β-1) * η_H + B_θ**-1 * (
+                            deriv_B_φ * η_A * (3 - β - v_r / (η_O + η_A)) +
+                            deriv_B_r * η_H * (v_r / (η_O + η_A) - 1)
+                        ) + deriv_B_φ / (B_θ * (η_O + η_A))
+                        * (v_r * η_A - η_H**2)
+                    ) + deriv_B_φ * (
+                        η_O - β * η_A + η_H**2 / (η_O + η_A) +
+                        2 * deriv_v_θ / (2*β - 1) +
+                        β * η_H * deriv_B_φ / B_θ +
+                        η_H / (B_θ * (η_O + η_A)) * (
+                            (β-1) * η_A + deriv_B_φ * v_r * (
+                                1 + 2 * η_A / (η_O + η_A)))))))
+
+    C_dash = dderiv_B_θ * (β-1) + (η_O + η_A)**-1 * (
+        2 * deriv_B_r * deriv_v_θ -
+        v_r * dderiv_B_θ + 2 * η_A * deriv_B_φ**2 / B_θ * (
+            β - 1 + deriv_B_r / B_θ - v_r / (η_O + η_A)
+        ) + η_H * deriv_B_φ * (
+            2 * (1 - deriv_B_r * (β-1) / B_θ) + (
+                deriv_B_r**2 + 2 * deriv_B_φ**2 * η_O / (η_O + η_A)
+            ) / B_θ**2
+        ) + 4*pi * ρ * v_r * η_H * (β-1) * (2*β-1) * (
+            η_O + η_A + η_H**2 / (η_O + η_A)
+        ) / (B_θ**2 + 4*pi * ρ * v_r * (β-1) * (2*β-1) * (
+            η_O + η_A + η_H**2 / (η_O + η_A))
+        ) * (
+            2 * dderiv_B_θ * (η_H * v_r / (η_O + η_A) + 2 * v_φ / (2*β-1)) -
+            deriv_B_r * (
+                v_φ + 2 * deriv_v_θ * η_H / (η_O + η_A) + (β-1) * η_H +
+                deriv_B_φ * (v_r * η_A - η_H**2) / (B_θ * (η_O + η_A)) +
+                B_θ**-1 * (
+                    deriv_B_φ * η_A * (3 - β - v_r / (η_O + η_A)) +
+                    deriv_B_r * η_H * (v_r / (η_O + η_A) - 1)
+                ) + B_θ * deriv_B_φ / (2*pi * ρ * (2*β-1) * v_r) +
+                B_θ**2 * deriv_B_φ * (deriv_B_r + (β-1) * B_θ) / (
+                    4 * pi**2 * ρ**2 * v_r * (β-1) * c_s**2 * (2*β-1)
+                )
+            ) - deriv_B_φ * (
+                η_O - β * η_A + η_H**2 / (η_O + η_A) +
+                2 * deriv_v_θ / (2*β-1) + β * η_H * deriv_B_φ / B_θ +
+                η_H * (
+                    η_A * (β-1) + deriv_B_φ * v_r * (1 - 2 * η_A / (η_O + η_A))
+                ) / (B_θ * (η_O + η_A)) - B_θ * (
+                    dderiv_B_θ + B_θ - B_θ / (c_s**2) * (
+                        4 * v_r * (β-1) * deriv_v_θ + deriv_B_φ**2 / (2*pi * ρ)
+                    )
+                ) / 4*pi * ρ * v_r * (β-1) * (2*β-1)
+            )
+        )
+    )
+
+    return (
+        4 * v_φ * A_dash + (
+            dderiv_B_θ * deriv_B_r + B_θ * C_dash +
+            2 * (β-1) * (B_θ * dderiv_B_θ + deriv_B_φ**2) +
+            B_θ * D * (deriv_B_r + (β-1) * B_θ)
+        ) / (2*pi * ρ)
+    ) / (
+        v_r * (4*β - 7) - 4 * v_φ * A -
+        B_θ * (C + 2 * (β-1) * (deriv_B_φ + (β-1) * B_θ))
     )
