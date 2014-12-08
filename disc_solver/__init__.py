0a7996bd1bd9b0c7e15cbc876aaa323004bb9185
James Tocknell <aragilar@gmail.com>
2014-12-08 19:33:28 +1100

    Moved out input stuff, and switched to namespaces

--- disc_solver/__init__.py
+++ disc_solver/__init__.py
@@ -13,10 +13,9 @@ mpl.use("Qt4Agg")
 mpl.rcParams["backend.qt4"] = "PySide"
 import matplotlib.pyplot as plt
 
-from .constants import AU, M_SUN, KM
 from .analyse import generate_plot
 from .solution import solution
-from .config import define_conditions
+from .config import define_conditions, get_input
 
 log = logbook.Logger(__name__)
 
@@ -25,52 +24,20 @@ def main():
     """
     The main function
     """
-
-    start = 90
-    stop = 85
-    taylor_stop_angle = 89.99
-
-    # pick a radii, 1AU makes it easy to calculate
-    radius = 1 * AU
-
-    central_mass = 1 * M_SUN
-
-    β = 4/3
-
-    v_rin_on_v_k = 0.1
-
-    # B_θ is the equipartition field
-    B_θ = 18  # G
-
-    # from wardle 2007 for 1 AU
-    # assume B ~ 1 G
-    η_O = 5e15  # cm^2/s
-    η_H = 5e16  # cm^2/s
-    η_A = 1e14  # cm^2/s
-
-    c_s = 0.99 * KM  # actually in cm/s for conversions
-    # ρ computed from Wardle 2007
-    ρ = 1.5e-9  # g/cm^3
-
-    (
-        angles, init_con, c_s, norm_kepler_sq, η_O, η_A, η_H, v_norm, B_norm,
-        ρ_norm
-    ) = define_conditions(
-        central_mass, radius, v_rin_on_v_k, B_θ, ρ, η_O, η_A, η_H, c_s, β,
-        start, stop
-    )
+    inp = get_input()
+    cons = define_conditions(inp)
 
     try:
         soln = solution(
-            angles, init_con, β, c_s, norm_kepler_sq, η_O,
-            η_A, η_H, max_steps=10000,
-            taylor_stop_angle=taylor_stop_angle / 180 * pi
+            cons.angles, cons.init_con, inp.β, cons.c_s, cons.norm_kepler_sq,
+            cons.η_O, cons.η_A, cons.η_H, max_steps=inp.max_steps,
+            taylor_stop_angle=inp.taylor_stop_angle / 180 * pi
         )
     except RuntimeError as e:
         # pylint: disable=no-member
         angles = e.x_vals
         soln = e.y_vals
 
-    fig = generate_plot(angles, soln, B_norm, v_norm, ρ_norm)
+    fig = generate_plot(angles, soln, cons.B_norm, cons.v_norm, cons.ρ_norm)
     plt.show()
     fig.savefig("plot.png")
--- disc_solver/config.py
+++ disc_solver/config.py
@@ -4,26 +4,25 @@ Define input and environment for ode system
 """
 
 from math import pi, sqrt
+from types import SimpleNamespace
 
 import logbook
 
 import numpy as np
 
-from .constants import G
+from .constants import G, AU, M_SUN, KM
 
 log = logbook.Logger(__name__)
 
 
-def define_conditions(
-    central_mass, radius, v_rin_on_v_k, B_θ, ρ, η_O, η_A, η_H, c_s, β, start,
-    stop
-):
+def define_conditions(inp):
     """
     Compute initial conditions based on input
     """
-    keplerian_velocity = sqrt(G * central_mass / radius)  # cm/s
+    cons = SimpleNamespace()
+    keplerian_velocity = sqrt(G * inp.central_mass / inp.radius)  # cm/s
 
-    v_r = - v_rin_on_v_k * keplerian_velocity
+    v_r = - inp.v_rin_on_v_k * keplerian_velocity
     if v_r > 0:
         log.error("v_r > 0")
         exit(1)
@@ -34,17 +33,17 @@ def define_conditions(
 
     # solution for A * v_φ**2 + B * v_φ + C = 0
     A_v_φ = 1
-    B_v_φ = - (v_r * η_H) / (2 * (η_O + η_A))
+    B_v_φ = - (v_r * inp.η_H) / (2 * (inp.η_O + inp.η_A))
     C_v_φ = (
-        v_r**2 / 2 + 2 * β * c_s**2 -
+        v_r**2 / 2 + 2 * inp.β * inp.c_s**2 -
         keplerian_velocity**2 +
-        B_θ**2 * (
-            2 * (β - 1) - v_r / (η_O + η_A)
-        ) / (4 * pi * ρ)
+        inp.B_θ**2 * (
+            2 * (inp.β - 1) - v_r / (inp.η_O + inp.η_A)
+        ) / (4 * pi * inp.ρ)
     )
     v_φ = - 1/2 * (B_v_φ - sqrt(B_v_φ**2 - 4 * A_v_φ * C_v_φ))
 
-    B_φ_prime = (v_φ * v_r * 4 * pi * ρ) / (2 * B_θ)
+    B_φ_prime = (v_φ * v_r * 4 * pi * inp.ρ) / (2 * inp.B_θ)
 
     log.debug("A_v_φ: {}".format(A_v_φ))
     log.debug("B_v_φ: {}".format(B_v_φ))
@@ -52,31 +51,66 @@ def define_conditions(
     log.info("v_φ: {}".format(v_r))
     log.info("B_φ_prime: {}".format(B_φ_prime))
 
-    v_norm = c_s
-    B_norm = B_θ
-    diff_norm = v_norm * radius
-
-    ρ_norm = B_norm**2 / (4 * pi * v_norm**2)
+    cons.v_norm = inp.c_s
+    cons.B_norm = inp.B_θ
+    cons.diff_norm = cons.v_norm * inp.radius
+    cons.ρ_norm = cons.B_norm**2 / (4 * pi * cons.v_norm**2)
 
     init_con = np.zeros(8)
 
-    init_con[0] = B_r / B_norm
-    init_con[1] = B_φ / B_norm
-    init_con[2] = B_θ / B_norm
-    init_con[3] = v_r / v_norm
-    init_con[4] = v_φ / v_norm
-    init_con[5] = v_θ / v_norm
-    init_con[6] = ρ / ρ_norm
-    init_con[7] = B_φ_prime / B_norm
+    init_con[0] = B_r / cons.B_norm
+    init_con[1] = B_φ / cons.B_norm
+    init_con[2] = inp.B_θ / cons.B_norm
+    init_con[3] = v_r / cons.v_norm
+    init_con[4] = v_φ / cons.v_norm
+    init_con[5] = v_θ / cons.v_norm
+    init_con[6] = inp.ρ / cons.ρ_norm
+    init_con[7] = B_φ_prime / cons.B_norm
 
-    norm_kepler_sq = keplerian_velocity**2 / v_norm**2
-    c_s = c_s / v_norm
-    η_O = η_O / diff_norm
-    η_A = η_A / diff_norm
-    η_H = η_H / diff_norm
+    cons.init_con = init_con
 
-    angles = np.linspace(start, stop, 10000) / 180 * pi
-    return (
-        angles, init_con, c_s, norm_kepler_sq, η_O, η_A, η_H, v_norm, B_norm,
-        ρ_norm
-    )
+    cons.norm_kepler_sq = keplerian_velocity**2 / cons.v_norm**2
+    cons.c_s = inp.c_s / cons.v_norm
+    cons.η_O = inp.η_O / cons.diff_norm
+    cons.η_A = inp.η_A / cons.diff_norm
+    cons.η_H = inp.η_H / cons.diff_norm
+
+    cons.angles = np.linspace(inp.start, inp.stop, inp.num_angles) / 180 * pi
+    return cons
+
+
+def get_input():
+    """
+    Get input values
+    """
+    inp = SimpleNamespace()
+    inp.start = 90
+    inp.stop = 85
+    inp.taylor_stop_angle = 89.99
+
+    # pick a radii, 1AU makes it easy to calculate
+    inp.radius = 1 * AU
+
+    inp.central_mass = 1 * M_SUN
+
+    inp.β = 4/3
+
+    inp.v_rin_on_v_k = 0.1
+
+    # B_θ is the equipartition field
+    inp.B_θ = 18  # G
+
+    # from wardle 2007 for 1 AU
+    # assume B ~ 1 G
+    inp.η_O = 5e15  # cm^2/s
+    inp.η_H = 5e16  # cm^2/s
+    inp.η_A = 1e14  # cm^2/s
+
+    inp.c_s = 0.99 * KM  # actually in cm/s for conversions
+    # ρ computed from Wardle 2007
+    inp.ρ = 1.5e-9  # g/cm^3
+
+    inp.max_steps = 10000
+    inp.num_angles = 10000
+
+    return inp
