6495c8768525afbae01a6391ce0069902440edc6
James Tocknell <aragilar@gmail.com>
2016-03-07 17:51:03 +1100

    Redo plot functions

--- src/disc_solver/analyse/plot_functions.py
+++ src/disc_solver/analyse/plot_functions.py
@@ -41,6 +41,140 @@ def generate_plot(soln_file, **kwargs):
     with_sonic = kwargs.pop("with sonic")
     stop = float(kwargs.pop("stop", 90))
 
+    indexes = degrees(angles) <= stop
+
+    param_names = [
+        {
+            "name": "B_r",
+            "y_label": "Magnetic Field (Gauss)",
+            "normalisation": B_norm,
+        },
+        {
+            "name": "B_φ",
+            "y_label": "Magnetic Field (Gauss)",
+            "normalisation": B_norm,
+        },
+        {
+            "name": "B_θ",
+            "y_label": "Magnetic Field (Gauss)",
+            "normalisation": B_norm,
+        },
+        {
+            "name": "v_r",
+            "y_label": "Velocity Field (km/s)",
+            "normalisation": v_norm / KM,  # km/s
+        },
+        {
+            "name": "v_φ",
+            "y_label": "Velocity Field (km/s)",
+            "normalisation": v_norm / KM,  # km/s
+            "offset": sqrt(cons.norm_kepler_sq) * v_norm / KM
+        },
+        {
+            "name": "v_θ",
+            "y_label": "Velocity Field (km/s)",
+            "normalisation": v_norm / KM,  # km/s
+            "legend": True,
+            "scale": kwargs.pop("v_θ scale", "linear"),
+            "extras": []
+        },
+        {
+            "name": "ρ",
+            "y_label": "Density ($g cm^{-3}$)",
+            "normalisation": ρ_norm,
+            "scale": "log",
+        },
+        {
+            "name": "B_φ_prime",
+            "y_label": "Magnetic Field (Gauss)",
+            "normalisation": B_norm,
+        },
+    ]
+
+    if with_slow:
+        param_names[5]["extras"].append({
+            "label": "slow",
+            "data": wave_speeds[MHD_WAVE_INDEX["slow"]],
+            "normalisation": v_norm / KM,
+        })
+    if with_alfven:
+        param_names[5]["extras"].append({
+            "label": "alfven",
+            "data": wave_speeds[MHD_WAVE_INDEX["alfven"]],
+            "normalisation": v_norm / KM,
+        })
+    if with_fast:
+        param_names[5]["extras"].append({
+            "label": "fast",
+            "data": wave_speeds[MHD_WAVE_INDEX["fast"]],
+            "normalisation": v_norm / KM,
+        })
+    if with_sonic:
+        param_names[5]["extras"].append({
+            "label": "sound",
+            "data": np.ones(len(soln)),
+            "normalisation": v_norm / KM,
+        })
+
+    fig, axes = plt.subplots(
+        nrows=2, ncols=4, tight_layout=True, sharex=True, **kwargs
+    )
+    axes.shape = len(param_names)
+    for i, settings in enumerate(param_names):
+        ax = axes[i]
+        ax.plot(
+            degrees(angles[indexes]),
+            (
+                soln[:, i] * settings["normalisation"] -
+                settings.get("offset", 0)
+            )[indexes], linestyle,
+        )
+        for extra in settings.get("extras", []):
+            ax.plot(
+                degrees(angles[indexes]),
+                (extra["data"] * extra["normalisation"])[indexes],
+                label=extra.get("label")
+            )
+        ax.set_xlabel("angle from plane (°)")
+        ax.set_ylabel(settings["y_label"])
+        ax.set_yscale(settings.get("scale", "linear"))
+        ax.set_title(settings["name"])
+        if settings.get("legend"):
+            ax.legend()
+        better_sci_format(ax.yaxis)
+    fig.suptitle("{}:{}".format(
+        soln_file.root.config_filename,
+        soln_file.root.config_input.label
+    ))
+    return fig
+
+
+def generate_plot_combine(soln_file, **kwargs):
+    """
+    Generate plot, with enough freedom to be able to format fig.
+    Combine velocities, fields onto on plot
+    """
+    soln_range = kwargs.pop("soln_range", "0")
+    soln_instance = get_solutions(soln_file, soln_range)
+    soln = soln_instance.solution
+    angles = soln_instance.angles
+    cons = soln_instance.initial_conditions
+    inp = soln_instance.soln_input
+
+    norms = get_normalisation(inp)  # need to allow config here
+    B_norm, v_norm, ρ_norm = norms["B_norm"], norms["v_norm"], norms["ρ_norm"]
+    zero_soln = np.zeros(len(soln))
+    v = np.array([zero_soln, zero_soln, soln[:, 5]])
+    wave_speeds = np.sqrt(mhd_wave_speeds(
+        v.T, soln[:, 0:3], soln[:, 6], cons.c_s * v_norm
+    ))
+    linestyle = kwargs.pop("line style")
+    with_slow = kwargs.pop("with slow")
+    with_alfven = kwargs.pop("with alfven")
+    with_fast = kwargs.pop("with fast")
+    with_sonic = kwargs.pop("with sonic")
+    stop = float(kwargs.pop("stop", 90))
+
     plot_props = OrderedDict([
         ("velocity", {
             "normalisation": v_norm / KM,  # km/s
