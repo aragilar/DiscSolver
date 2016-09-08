Fix equations and normalisation

src/disc_solver/analyse/combine_plot.py
@@ -110,8 +110,7 @@ def generate_plot_combine(
     zero_soln = np.zeros(len(solution))
     v = np.array([zero_soln, zero_soln, solution[:, ODEIndex.v_θ]])
     wave_speeds = np.sqrt(mhd_wave_speeds(
-        v.T, solution[:, MAGNETIC_INDEXES], solution[:, ODEIndex.ρ],
-        cons.c_s * v_norm
+        v.T, solution[:, MAGNETIC_INDEXES], solution[:, ODEIndex.ρ], v_norm
     ))
 
     plot_props = OrderedDict([

src/disc_solver/solve/config.py
@@ -3,7 +3,7 @@
 Define input and environment for ode system
 """
 
-from math import pi, sqrt
+from math import sqrt
 
 import logbook
 
@@ -23,42 +23,32 @@ def define_conditions(inp):
     """
     Compute initial conditions based on input
     """
-    ρ = 1  # ρ is always normalised by itself
-    c_s = 1  # velocities normalised by c_s, so c_s = 1
+    ρ = 1
+    B_θ = 1
+    v_θ = 0
+    B_r = 0
+    B_φ = 0
 
-    v_θ = 0  # symmetry across disc
-    B_r = 0  # symmetry across disc
-    B_φ = 0  # symmetry across disc
-
-    v_r = - inp.v_rin_on_c_s  # velocities normalised by c_s
-    B_θ = inp.v_a_on_c_s
+    v_r = - inp.v_rin_on_c_s
+    a_0 = inp.v_a_on_c_s ** 2
+    norm_kepler_sq = 1 / inp.c_s_on_v_k ** 2
 
     β = inp.β
-    norm_kepler_sq = 1 / inp.c_s_on_v_k ** 2
     η_O = inp.η_O
     η_A = inp.η_A
     η_H = inp.η_H
 
-    # solution for v_φ**2 + B * v_φ + C = 0
-    B_v_φ = (inp.v_rin_on_c_s * η_H) / (2 * (η_O + η_A))
-    C_v_φ = (
-        inp.v_rin_on_c_s**2 / 2 + 2 * β - norm_kepler_sq -
-        inp.v_a_on_c_s**2 * inp.v_rin_on_c_s / (η_O + η_A)
-    )
-    log.debug("B_v_φ: {}".format(B_v_φ))
-    log.debug("C_v_φ: {}".format(C_v_φ))
-
     v_φ = (
-        (inp.v_rin_on_c_s * η_H) / (4 * (η_O + η_A)) + sqrt(
-            norm_kepler_sq - inp.v_rin_on_c_s**2 / 2 * (
-                1 - η_H**2 / (8 * (η_O + η_A)**2)
-            ) - inp.v_rin_on_c_s * inp.v_a_on_c_s**2 / (η_O + η_A) - 2 * β
+        - v_r * η_H / (4 * (η_O + η_A)) + sqrt(
+            norm_kepler_sq - 2 * β + v_r * (
+                a_0 / (η_O + η_A) + v_r / 2 * (
+                    η_H ** 2 / (8 * (η_O + η_A) ** 2) - 1
+                )
+            )
         )
     )
 
-    B_φ_prime = (
-        v_φ * v_r * 2 * pi * ρ
-    ) / B_θ
+    B_φ_prime = v_φ * v_r / (2 * a_0)
 
     init_con = np.zeros(11)
 
@@ -77,7 +67,7 @@ def define_conditions(inp):
     angles = np.radians(np.linspace(inp.start, inp.stop, inp.num_angles))
 
     return InitialConditions(
-        norm_kepler_sq=norm_kepler_sq, c_s=c_s, init_con=init_con,
+        norm_kepler_sq=norm_kepler_sq, a_0=a_0, init_con=init_con,
         angles=angles, β=β
     )
 

tests/test_odes.py
@@ -16,10 +16,10 @@ ODE_NUMBER = 11
 class ODE_Test(unittest.TestCase):
     def setUp(self):
         # These are all arbitrary values, this should not affect the result
-        params = np.array([1 for i in range(ODE_NUMBER)])
+        params = np.array([7 for i in range(ODE_NUMBER)])
         self.β = 2
-        self.c_s = 2
-        self.central_mass = 2
+        self.norm_kepler_sq = 2
+        self.a_0 = 2
 
         # This is slightly off the plane, this should mean we don't get
         # cancellation
@@ -29,7 +29,7 @@ class ODE_Test(unittest.TestCase):
         derivs = np.zeros(ODE_NUMBER)
         with logbook.NullHandler().applicationbound():
             self.rhs, internal_data = ode_system(
-                self.β, self.c_s, self.central_mass, params, with_taylor=False
+                self.β, self.a_0, self.norm_kepler_sq, params, with_taylor=False
             )
             self.rhs(self.angle, params, derivs)
 
@@ -85,8 +85,8 @@ class ODE_Test(unittest.TestCase):
 
     def test_radial_momentum(self):
         eqn = (self.v_θ * self.deriv_v_r - 1/2 * self.v_r**2 -
-            self.v_θ**2 - self.v_φ**2 + self.central_mass -
-            self.c_s**2 * 2 * self.β - 1/(4 * pi * self.ρ) * (
+            self.v_θ**2 - self.v_φ**2 + self.norm_kepler_sq -
+            2 * self.β - self.a_0 / self.ρ * (
                 self.B_θ * self.deriv_B_r + (self.β - 1) * (
                     self.B_θ**2 + self.B_φ**2
                 )
@@ -97,23 +97,24 @@ class ODE_Test(unittest.TestCase):
 
     def test_azimuthal_mometum(self):
         eqn = (self.v_θ * self.deriv_v_φ + 1/2 * self.v_r * self.v_φ -
-            tan(self.angle) * self.v_θ * self.v_φ - 
-            1 / (4 * pi * self.ρ) * (self.B_θ * self.deriv_B_φ +
+            tan(self.angle) * self.v_θ * self.v_φ - self.a_0 / self.ρ * (
+                self.B_θ * self.deriv_B_φ +
                 (1 - self.β) * self.B_r * self.B_φ -
                 tan(self.angle) * self.B_θ * self.B_φ
-        ))
+            )
+        )
         print("azimuthal momentum", eqn)
         self.assertAlmostEqual(0, eqn)
 
     def test_polar_momentum(self):
         eqn = (self.v_r * self.v_θ / 2 + self.v_θ * self.deriv_v_θ +
-            tan(self.angle) * self.v_φ ** 2 +
-            self.c_s ** 2 * self.deriv_ρ / self.ρ +
-            1 / (4 * pi * self.ρ) * (
+            tan(self.angle) * self.v_φ ** 2 + self.deriv_ρ / self.ρ +
+            self.a_0 / self.ρ * (
                 (self.β - 1) * self.B_θ * self.B_r +
                 self.B_r * self.deriv_B_r + self.B_φ * self.deriv_B_φ -
                 self.B_φ ** 2 * tan(self.angle)
-        ))
+            )
+        )
         print("polar momentum", eqn)
         self.assertAlmostEqual(0, eqn)
 
@@ -141,7 +142,7 @@ class ODE_Test(unittest.TestCase):
         self.assertAlmostEqual(0, eqn)
 
     def test_azimuthal_induction_numeric(self):
-        step = - 1e-4
+        step = 1e-4
         new_params = self.params + self.derivs * step
         new_angle = self.angle + step
         new_derivs = np.zeros(ODE_NUMBER)