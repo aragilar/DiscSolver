c5703745f5ae9582ad11b4924eb48b49d5d0ec91
James Tocknell <aragilar@gmail.com>
2017-01-06 11:47:27 +1100

    Fix slice in mcmc solver

--- src/disc_solver/solve/mcmc.py
+++ src/disc_solver/solve/mcmc.py
@@ -135,7 +135,7 @@ def get_logprob_of_soln(new_soln_input, soln):
         return - float("inf")
     return - (
         new_soln_input.target_velocity - soln.solution[-1, ODEIndex.v_θ]
-    ) ** 2 - diff(diff(soln.solution[:, ODEIndex.v_θ])[0, -1])
+    ) ** 2 - diff(diff(soln.solution[:, ODEIndex.v_θ])[(0, -1)])
 
 
 def generate_initial_positions(soln_input):
