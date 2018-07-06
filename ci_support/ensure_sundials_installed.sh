b845985d8b62c652d7adfd2e708534e0db558493
James Tocknell <aragilar@gmail.com>
2018-07-06 11:34:21 +1000

    Download sundials as needed for travis

---
+++ ci_support/ensure_sundials_installed.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# from https://github.com/bmcage/odes/
+
+SUNDIALS_DEFAULT_VERSION='2.7.0'
+SUNDIALS_DEFAULT_PRECISION='double'
+
+export SUNDIALS_DIR=$HOME/sundials/"${SUNDIALS_VERSION:-$SUNDIALS_DEFAULT_VERSION}"/"${SUNDIALS_PRECISION:-$SUNDIALS_DEFAULT_PRECISION}"
+SUNDIALS_LIBDIR=$SUNDIALS_DIR/lib
+SUNDIALS_INCLUDEDIR=$SUNDIALS_DIR/include
+
+if [ ! -d "$SUNDIALS_LIBDIR" ]; then
+    mkdir -p $SUNDIALS_DIR
+    echo "Installing sundials ${SUNDIALS_VERSION:-$SUNDIALS_DEFAULT_VERSION}"
+    ./ci_support/install_sundials.sh
+else
+    echo "Using cached sundials"
+fi
+
+export LD_LIBRARY_PATH=$SUNDIALS_LIBDIR:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$SUNDIALS_LIBDIR:$LIBRARY_PATH
+export CPATH=$SUNDIALS_INCLUDEDIR:$CPATH
