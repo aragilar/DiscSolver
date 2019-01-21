9d379c39aaf8f7c1977a81d91ee38c03e99bb8ca
James Tocknell <aragilar@gmail.com>
2019-01-21 18:17:46 +1100

    Add initial support for setting mcmc variables

--- src/disc_solver/solve/mcmc.py
+++ src/disc_solver/solve/mcmc.py
@@ -3,7 +3,7 @@
 Stepper related logic
 """
 
-from enum import IntEnum, unique
+from enum import IntEnum, unique, auto
 
 import logbook
 
@@ -26,32 +26,46 @@ TARGETED_PROB_WEIGHTING = 1
 OUTFLOW_RATE_PROB_WEIGHTING = 1
 
 
-@unique
-class SysVars(IntEnum):
+def generate_sysvars_enum(soln_input):
     """
-    Variables for MCMC
+    Create SysVars enum based on which parameters to mcmc
     """
-    γ = 0
-    v_rin_on_c_s = 1
-    v_a_on_c_s = 2
-    c_s_on_v_k = 3
+    if getattr(soln_input, "mcmc_vars", None) is None:
+        with_v_r = True
+        with_v_a = True
+        with_v_k = True
+
+    @unique
+    class SysVars(IntEnum):
+        """
+        Variables for MCMC
+        """
+        γ = 0
+        if with_v_r:
+            v_rin_on_c_s = auto()
+        if with_v_a:
+            v_a_on_c_s = auto()
+        if with_v_k:
+            c_s_on_v_k = auto()
+
+    return SysVars
 
 
-def solution_input_to_sys_vars(soln_input):
+def solution_input_to_sys_vars(soln_input, sys_vars_enum):
     """
     Convert instance of SolutionInput to mcmc variables
     """
     return [
-        getattr(soln_input, v.name) for v in sorted(SysVars)
+        getattr(soln_input, v.name) for v in sorted(sys_vars_enum)
     ]
 
 
-def sys_vars_to_solution_input(sys_vars, old_soln_input):
+def sys_vars_to_solution_input(sys_vars, old_soln_input, sys_vars_enum):
     """
     Convert mcmc variables to instance of SolutionInput
     """
     soln_input = SolutionInput(**vars(old_soln_input))
-    for v in SysVars:
+    for v in sys_vars_enum:
         setattr(soln_input, v.name, sys_vars[v])
     return soln_input
 
@@ -60,16 +74,18 @@ def solver(soln_input, run, store_internal=True):
     """
     MCMC solver
     """
+    sys_vars_enum = generate_sysvars_enum(soln_input)
     logprobfunc = LogProbGenerator(
-        soln_input, run, store_internal=store_internal
+        soln_input, run, store_internal=store_internal,
+        sys_vars_enum=sys_vars_enum,
     )
     sampler = emcee.EnsembleSampler(
-        soln_input.nwalkers, len(SysVars), logprobfunc,
+        soln_input.nwalkers, len(sys_vars_enum), logprobfunc,
     )
 
     with errstate(invalid="ignore"):
         sampler.run_mcmc(
-            generate_initial_positions(soln_input),
+            generate_initial_positions(soln_input, sys_vars_enum),
             soln_input.iterations
         )
     run.final_solution = logprobfunc.best_solution
@@ -80,7 +96,7 @@ class LogProbGenerator:
     Generate log probability function
     """
     # pylint: disable=too-few-public-methods
-    def __init__(self, soln_input, run, *, store_internal):
+    def __init__(self, soln_input, run, *, store_internal, sys_vars_enum):
         self._soln_input = soln_input
         self._run = run
         self._store_internal = store_internal
@@ -89,9 +105,12 @@ class LogProbGenerator:
         self._root_func, self._root_func_args = velocity_stop_generator(
             self._soln_input
         )
+        self._sys_vars_enum = sys_vars_enum
 
     def __call__(self, sys_vars):
-        new_soln_input = sys_vars_to_solution_input(sys_vars, self._soln_input)
+        new_soln_input = sys_vars_to_solution_input(
+            sys_vars, self._soln_input, self._sys_vars_enum
+        )
         if not solution_input_valid(new_soln_input):
             log.info("MCMC input invalid")
             return - float("inf")
@@ -156,13 +175,13 @@ def get_outflow_rate_probability(soln):
     return - sum(diff(Δ_v_θ))
 
 
-def generate_initial_positions(soln_input):
+def generate_initial_positions(soln_input, sys_vars_enum):
     """
     Generate initial positions of walkers
     """
-    sys_vars = solution_input_to_sys_vars(soln_input)
+    sys_vars = solution_input_to_sys_vars(soln_input, sys_vars_enum)
     return sys_vars * (
-        INITIAL_SPREAD * randn(soln_input.nwalkers, len(SysVars)) + 1
+        INITIAL_SPREAD * randn(soln_input.nwalkers, len(sys_vars_enum)) + 1
     )
 
 
