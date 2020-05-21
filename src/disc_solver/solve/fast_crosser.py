87edfdb96413a3f3e79d45d789da30dafaf245ab
James Tocknell <aragilar@gmail.com>
2020-05-21 14:14:55 +1000

    Use golden ratio to do bisection to cross fast speed

--- src/disc_solver/solve/fast_crosser.py
+++ src/disc_solver/solve/fast_crosser.py
@@ -6,13 +6,12 @@ from enum import Enum
 
 import logbook
 
-from numpy import max as np_max
+from numpy import max as np_max, sqrt
 
 from .single import solver as single_solver
 from .stepper import (
     solver as step_solver, writer_generator, cleanup_generator,
-    binary_searcher, stepper_creator, alternate_cleanup_generator,
-    StepperStop, StepperError,
+    binary_searcher, alternate_cleanup_generator, StepperStop, StepperError,
 )
 
 from ..analyse.utils import get_mach_numbers
@@ -24,39 +23,144 @@ DEFAULT_NUM_ATTEMPTS = 1000
 DEFAULT_MAX_SEARCH_STEPS = 200
 DEFAULT_FINAL_STOP = 90
 DEFAULT_FINAL_STEPS = 900000
+GOLDEN_RATIO = (1 + sqrt(5)) / 2
 
 
-class SplitterStatus(Enum):
+class Phase(Enum):
     """
-    Status Enum for which way to the best solution
+    The phases in which the stepper can be in
     """
-    STOP = "STOP"
-    INCREASE = "increase"
-    DECREASE = "decrease"
+    STARTING = "starting"
+    FINDING_MAX = "finding max"
+    FINDING_MIN = "finding min"
+    BISECTING = "bisecting"
 
 
-def step_input():
+def compute_quality(soln):
     """
-    Create new input for next step
+    Compute the value for comparing the solutions
     """
-    def step_func(soln, soln_type, step_size):
+    _, _, _, fast_mach = get_mach_numbers(soln)
+    return np_max(fast_mach)
+
+
+def stepper_creator(soln_writer, *, step_size, solution_input):
+    """
+    Create stepper
+    """
+    def get_next_input(current_input, new_v_r):
+        current_input.update(
+            stop=solution_input.stop, num_angles=solution_input.num_angles,
+            v_rin_on_c_s=new_v_r,
+        )
+        return current_input
+
+    limits = {
+        "min": None,
+        "min_val": None,
+        "max": None,
+        "max_val": None,
+        "mid": None,
+        "mid_val": None,
+    }
+    phase = Phase.STARTING
+
+    def stepper(soln, attempt):
         """
-        Return new input
+        Stepper func
         """
+        nonlocal phase
+        soln_writer(soln, attempt)
         inp_dict = vars(soln.solution_input)
-        prev_v_r = inp_dict["v_rin_on_c_s"]
-        if soln_type == SplitterStatus.DECREASE:
-            inp_dict["v_rin_on_c_s"] -= step_size
-        elif soln_type == SplitterStatus.INCREASE:
-            inp_dict["v_rin_on_c_s"] += step_size
-        elif soln_type == SplitterStatus.STOP:
-            raise StepperStop("Stepper stopped")
+        current_v_r = inp_dict["v_rin_on_c_s"]
+        current_quality = compute_quality(soln)
+        log.notice("v_r is {}", current_v_r)
+
+        if phase == Phase.STARTING:
+            log.notice("starting search for limits")
+            limits["min"] = current_v_r
+            limits["max"] = current_v_r
+            limits["min_val"] = current_quality
+            limits["max_val"] = current_quality
+            phase = Phase.FINDING_MAX
+            log.notice("starting search for max limit")
+            return SolutionInput(**get_next_input(
+                inp_dict, inp_dict["v_rin_on_c_s"] + step_size
+            ))
+
+        if phase == Phase.FINDING_MAX:
+            if current_quality > limits["max_val"]:
+                limits["max"] = current_v_r
+                limits["max_val"] = current_quality
+                log.notice("searching for max limit on attempt {}", attempt)
+                return SolutionInput(**get_next_input(
+                    inp_dict, inp_dict["v_rin_on_c_s"] + step_size
+                ))
+            phase = Phase.FINDING_MIN
+            log.notice("starting search for min limit")
+            return SolutionInput(**get_next_input(
+                inp_dict, limits["min"] - step_size
+            ))
+
+        if phase == Phase.FINDING_MIN:
+            if current_quality > limits["min_val"]:
+                limits["min"] = current_v_r
+                limits["min_val"] = current_quality
+                log.notice("searching for min limit on attempt {}", attempt)
+                return SolutionInput(**get_next_input(
+                    inp_dict, inp_dict["v_rin_on_c_s"] - step_size
+                ))
+            phase = Phase.BISECTING
+            log.notice(
+                "found limits {} {}, bisecting", limits["min"], limits["max"]
+            )
+            if limits["min"] == limits["max"]:
+                raise StepperError("Unable to step off value")
+            # This sets up the golden-section search by starting with the
+            # golden ratio
+            return SolutionInput(**get_next_input(
+                inp_dict, limits["min"] + (
+                    (limits["min"] - limits["max"]) / (GOLDEN_RATIO - 1)
+                )
+            ))
+        if phase == Phase.BISECTING:
+            log.notice("bisecting on attempt {}", attempt)
+            if limits["mid"] is None:
+                limits["mid"] = current_v_r
+                limits["mid_val"] = current_quality
+                return SolutionInput(**get_next_input(
+                    inp_dict, limits["min"] + (limits["max"] - limits["mid"])
+                ))
+            if limits["mid"] > current_v_r:
+                lmid_lim = current_v_r
+                hmid_lim = limits["mid"]
+                lmid_val = current_quality
+                hmid_val = limits["mid_val"]
+            else:
+                hmid_lim = current_v_r
+                lmid_lim = limits["mid"]
+                hmid_val = current_quality
+                lmid_val = limits["mid_val"]
+            if lmid_val > hmid_val:
+                limits["mid"] = lmid_lim
+                limits["mid_val"] = lmid_val
+                limits["max"] = hmid_lim
+                limits["max_val"] = hmid_val
+            else:
+                limits["min"] = lmid_lim
+                limits["min_val"] = lmid_val
+                limits["mid"] = hmid_lim
+                limits["mid_val"] = hmid_val
+            new_v_r = limits["min"] + (
+                limits["max"] - limits["mid"]
+            )
+            if new_v_r == current_v_r:
+                StepperStop("Hit numerical limit")
+            return SolutionInput(**get_next_input(inp_dict, new_v_r))
         else:
-            raise StepperError("Solution type not known")
-        if prev_v_r == inp_dict["v_rin_on_c_s"]:
-            raise StepperStop("Hit numerical limit")
-        return SolutionInput(**inp_dict)
-    return step_func
+            raise StepperError("Unknown phase")
+
+    return stepper
 
 
 def solution_generator(
@@ -90,71 +194,15 @@ def solution_generator(
     return solution_func
 
 
-class SolutionSplitter:
-    """
-    Work out which way to look for best solution
-    """
-    # pylint: disable=too-few-public-methods
-    def __init__(self):
-        self._previous_value = None
-        self._previous_direction = None
-
-    def _get_direction(self, soln):
-        """
-        Work out which way to look for best solution
-        """
-        _, _, _, fast_mach = get_mach_numbers(soln)
-        current = np_max(fast_mach)
-
-        if self._previous_value is None:
-            self._previous_value = current
-            self._previous_direction = SplitterStatus.INCREASE
-            return self._previous_direction
-        if self._previous_value > current and (
-            self._previous_direction == SplitterStatus.DECREASE
-        ):
-            self._previous_value = current
-            self._previous_direction = SplitterStatus.INCREASE
-            return self._previous_direction
-        if self._previous_value > current and (
-            self._previous_direction == SplitterStatus.INCREASE
-        ):
-            self._previous_value = current
-            self._previous_direction = SplitterStatus.DECREASE
-            return self._previous_direction
-        if self._previous_value < current and (
-            self._previous_direction == SplitterStatus.DECREASE
-        ):
-            self._previous_value = current
-            self._previous_direction = SplitterStatus.DECREASE
-            return self._previous_direction
-        if self._previous_value < current and (
-            self._previous_direction == SplitterStatus.INCREASE
-        ):
-            self._previous_value = current
-            self._previous_direction = SplitterStatus.INCREASE
-            return self._previous_direction
-
-        # increase by default
-        self._previous_value = current
-        self._previous_direction = SplitterStatus.INCREASE
-        return self._previous_direction
-
-    def __call__(self, soln):
-        return soln, self._get_direction(soln)
-
-
 def solver(
     soln_input, run, *, store_internal=True, final_stop=DEFAULT_FINAL_STOP,
-    final_steps=DEFAULT_FINAL_STEPS, max_search_steps=DEFAULT_MAX_SEARCH_STEPS,
-    num_attempts=DEFAULT_NUM_ATTEMPTS,
+    final_steps=DEFAULT_FINAL_STEPS, num_attempts=DEFAULT_NUM_ATTEMPTS,
 ):
     """
     fast-crosser solver
 
     uses step solver and single solver to cross fast speed
     """
-    step_func = step_input()
     writer = writer_generator(run)
     cleanup = cleanup_generator(
         run, writer, no_final_solution=True,
@@ -165,10 +213,8 @@ def solver(
             final_steps=final_steps,
         ), cleanup,
         stepper_creator(
-            writer, step_func,
-            SolutionSplitter(),
-            max_search_steps=max_search_steps,
-            initial_step_size=0.01 * soln_input.v_rin_on_c_s
+            writer, step_size=0.01 * soln_input.v_rin_on_c_s,
+            solution_input=soln_input
         ),
         alternate_cleanup_generator(run, no_final_solution=True),
         soln_input, num_attempts=num_attempts,
