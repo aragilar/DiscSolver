140c22e4b515b31d1079577bd59a542528db9198
Restyled.io <commits@restyled.io>
2020-08-16 02:35:23 +0000

    Restyled by black

--- src/disc_solver/solve/config.py
+++ src/disc_solver/solve/config.py
@@ -23,11 +23,10 @@ def v_φ_boundary_func(*, v_r, η_H, η_P, norm_kepler_sq, a_0, γ):
     """
     try:
         return v_r * η_H / (4 * η_P) + sqrt(
-            norm_kepler_sq - 5/2 + 2 * γ + v_r * (
-                a_0 / η_P + v_r / 2 * (
-                    η_H ** 2 / (8 * η_P ** 2) - 1
-                )
-            )
+            norm_kepler_sq
+            - 5 / 2
+            + 2 * γ
+            + v_r * (a_0 / η_P + v_r / 2 * (η_H ** 2 / (8 * η_P ** 2) - 1))
         )
     except ValueError:
         raise SolverError("Input implies complex v_φ")
@@ -47,7 +46,7 @@ def E_r_boundary_func(*, v_r, v_φ, η_P, η_H, η_perp_sq, a_0):
     """
     Compute E_r at the midplane
     """
-    return - v_φ - v_r / η_P * (η_H + η_perp_sq * v_φ / (2 * a_0))
+    return -v_φ - v_r / η_P * (η_H + η_perp_sq * v_φ / (2 * a_0))
 
 
 def define_conditions(inp):
@@ -60,7 +59,7 @@ def define_conditions(inp):
     B_r = float_type(0)
     B_φ = float_type(0)
 
-    v_r = - inp.v_rin_on_c_s
+    v_r = -inp.v_rin_on_c_s
     a_0 = inp.v_a_on_c_s ** 2
     norm_kepler_sq = 1 / inp.c_s_on_v_k ** 2
 
@@ -103,8 +102,7 @@ def define_conditions(inp):
         raise SolverError("Input implies NaN")
 
     return InitialConditions(
-        norm_kepler_sq=norm_kepler_sq, a_0=a_0, init_con=init_con,
-        angles=angles, γ=γ
+        norm_kepler_sq=norm_kepler_sq, a_0=a_0, init_con=init_con, angles=angles, γ=γ
     )
 
 
@@ -117,64 +115,51 @@ def get_input_from_conffile(*, config_file, overrides=None):
         with config_file.open("r") as f:
             config.read_file(f)
 
-    return add_overrides(overrides=overrides, config_input=ConfigInput(
-        start=config.get("config", "start", fallback="0"),
-        stop=config.get("config", "stop", fallback="5"),
-        taylor_stop_angle=config.get(
-            "config", "taylor_stop_angle", fallback="0.001"
+    return add_overrides(
+        overrides=overrides,
+        config_input=ConfigInput(
+            start=config.get("config", "start", fallback="0"),
+            stop=config.get("config", "stop", fallback="5"),
+            taylor_stop_angle=config.get(
+                "config", "taylor_stop_angle", fallback="0.001"
+            ),
+            max_steps=config.get("config", "max_steps", fallback="10000"),
+            num_angles=config.get("config", "num_angles", fallback="10000"),
+            label=config.get("config", "label", fallback="default"),
+            relative_tolerance=config.get(
+                "config", "relative_tolerance", fallback="1e-6"
+            ),
+            absolute_tolerance=config.get(
+                "config", "absolute_tolerance", fallback="1e-10"
+            ),
+            jump_before_sonic=config.get(
+                "config", "jump_before_sonic", fallback="None"
+            ),
+            v_θ_sonic_crit=config.get("config", "v_θ_sonic_crit", fallback="None"),
+            after_sonic=config.get("config", "after_sonic", fallback="None"),
+            interp_range=config.get("config", "interp_range", fallback="10"),
+            sonic_interp_size=config.get(
+                "config", "sonic_interp_size", fallback="None"
+            ),
+            interp_slice=config.get("config", "interp_slice", fallback="-1000,-100"),
+            η_derivs=config.get("config", "η_derivs", fallback="True"),
+            nwalkers=config.get("config", "nwalkers", fallback="8"),
+            iterations=config.get("config", "iterations", fallback="3"),
+            threads=config.get("config", "threads", fallback="1"),
+            mcmc_vars=config.get("config", "mcmc_vars", fallback="v_r,v_a,v_k"),
+            target_velocity=config.get("config", "target_velocity", fallback="0.9"),
+            split_method=config.get("config", "split_method", fallback="v_θ_deriv"),
+            use_taylor_jump=config.get("config", "use_taylor_jump", fallback="True"),
+            use_E_r=config.get("config", "use_E_r", fallback="False"),
+            γ=config.get("initial", "γ", fallback="0.001"),
+            v_rin_on_c_s=config.get("initial", "v_rin_on_c_s", fallback="1"),
+            v_a_on_c_s=config.get("initial", "v_a_on_c_s", fallback="1"),
+            c_s_on_v_k=config.get("initial", "c_s_on_v_k", fallback="0.03"),
+            η_O=config.get("initial", "η_O", fallback="0.001"),
+            η_H=config.get("initial", "η_H", fallback="0.0001"),
+            η_A=config.get("initial", "η_A", fallback="0.0005"),
         ),
-        max_steps=config.get("config", "max_steps", fallback="10000"),
-        num_angles=config.get("config", "num_angles", fallback="10000"),
-        label=config.get("config", "label", fallback="default"),
-        relative_tolerance=config.get(
-            "config", "relative_tolerance", fallback="1e-6"
-        ),
-        absolute_tolerance=config.get(
-            "config", "absolute_tolerance", fallback="1e-10"
-        ),
-        jump_before_sonic=config.get(
-            "config", "jump_before_sonic", fallback="None"
-        ),
-        v_θ_sonic_crit=config.get(
-            "config", "v_θ_sonic_crit", fallback="None"
-        ),
-        after_sonic=config.get(
-            "config", "after_sonic", fallback="None"
-        ),
-        interp_range=config.get(
-            "config", "interp_range", fallback="10"
-        ),
-        sonic_interp_size=config.get(
-            "config", "sonic_interp_size", fallback="None"
-        ),
-        interp_slice=config.get(
-            "config", "interp_slice", fallback="-1000,-100"
-        ),
-        η_derivs=config.get("config", "η_derivs", fallback="True"),
-        nwalkers=config.get("config", "nwalkers", fallback="8"),
-        iterations=config.get("config", "iterations", fallback="3"),
-        threads=config.get("config", "threads", fallback="1"),
-        mcmc_vars=config.get("config", "mcmc_vars", fallback="v_r,v_a,v_k"),
-        target_velocity=config.get(
-            "config", "target_velocity", fallback="0.9"
-        ),
-        split_method=config.get(
-            "config", "split_method", fallback="v_θ_deriv"
-        ),
-        use_taylor_jump=config.get(
-            "config", "use_taylor_jump", fallback="True"
-        ),
-        use_E_r=config.get(
-            "config", "use_E_r", fallback="False"
-        ),
-        γ=config.get("initial", "γ", fallback="0.001"),
-        v_rin_on_c_s=config.get("initial", "v_rin_on_c_s", fallback="1"),
-        v_a_on_c_s=config.get("initial", "v_a_on_c_s", fallback="1"),
-        c_s_on_v_k=config.get("initial", "c_s_on_v_k", fallback="0.03"),
-        η_O=config.get("initial", "η_O", fallback="0.001"),
-        η_H=config.get("initial", "η_H", fallback="0.0001"),
-        η_A=config.get("initial", "η_A", fallback="0.0005"),
-    ))
+    )
 
 
 def new_inputs_with_overrides(config_input, solution_input, overrides):
@@ -184,9 +169,7 @@ def new_inputs_with_overrides(config_input, solution_input, overrides):
     """
     if overrides is None:
         overrides = {}
-    new_config_input = add_overrides(
-        config_input=config_input, overrides=overrides,
-    )
+    new_config_input = add_overrides(config_input=config_input, overrides=overrides,)
     new_soln_input = new_config_input.to_soln_input()
 
     initial_soln_input = config_input.to_soln_input()
--- src/disc_solver/solve/fast_crosser.py
+++ src/disc_solver/solve/fast_crosser.py
@@ -11,8 +11,13 @@ from numpy import max as np_max, sqrt
 from .single import solver as single_solver
 from .solution import get_known_broken_solution
 from .stepper import (
-    solver as step_solver, writer_generator, cleanup_generator,
-    binary_searcher, alternate_cleanup_generator, StepperStop, StepperError,
+    solver as step_solver,
+    writer_generator,
+    cleanup_generator,
+    binary_searcher,
+    alternate_cleanup_generator,
+    StepperStop,
+    StepperError,
 )
 from .utils import SolverError
 
@@ -32,6 +37,7 @@ class Phase(Enum):
     """
     The phases in which the stepper can be in
     """
+
     STARTING = "starting"
     FINDING_MAX = "finding max"
     FINDING_MIN = "finding min"
@@ -50,9 +56,11 @@ def stepper_creator(soln_writer, *, step_size, solution_input):
     """
     Create stepper
     """
+
     def get_next_input(current_input, new_v_r):
         current_input.update(
-            stop=solution_input.stop, num_angles=solution_input.num_angles,
+            stop=solution_input.stop,
+            num_angles=solution_input.num_angles,
             v_rin_on_c_s=new_v_r,
         )
         return current_input
@@ -86,53 +94,53 @@ def stepper_creator(soln_writer, *, step_size, solution_input):
             limits["max_val"] = current_quality
             phase = Phase.FINDING_MAX
             log.notice("starting search for max limit")
-            return SolutionInput(**get_next_input(
-                inp_dict, inp_dict["v_rin_on_c_s"] + step_size
-            ))
+            return SolutionInput(
+                **get_next_input(inp_dict, inp_dict["v_rin_on_c_s"] + step_size)
+            )
 
         if phase == Phase.FINDING_MAX:
             if current_quality > limits["max_val"]:
                 limits["max"] = current_v_r
                 limits["max_val"] = current_quality
                 log.notice("searching for max limit on attempt {}", attempt)
-                return SolutionInput(**get_next_input(
-                    inp_dict, inp_dict["v_rin_on_c_s"] + step_size
-                ))
+                return SolutionInput(
+                    **get_next_input(inp_dict, inp_dict["v_rin_on_c_s"] + step_size)
+                )
             phase = Phase.FINDING_MIN
             log.notice("starting search for min limit")
-            return SolutionInput(**get_next_input(
-                inp_dict, limits["min"] - step_size
-            ))
+            return SolutionInput(**get_next_input(inp_dict, limits["min"] - step_size))
 
         if phase == Phase.FINDING_MIN:
             if current_quality > limits["min_val"]:
                 limits["min"] = current_v_r
                 limits["min_val"] = current_quality
                 log.notice("searching for min limit on attempt {}", attempt)
-                return SolutionInput(**get_next_input(
-                    inp_dict, inp_dict["v_rin_on_c_s"] - step_size
-                ))
+                return SolutionInput(
+                    **get_next_input(inp_dict, inp_dict["v_rin_on_c_s"] - step_size)
+                )
             phase = Phase.BISECTING
-            log.notice(
-                "found limits {} {}, bisecting", limits["min"], limits["max"]
-            )
+            log.notice("found limits {} {}, bisecting", limits["min"], limits["max"])
             if limits["min"] == limits["max"]:
                 raise StepperError("Unable to step off value")
             # This sets up the golden-section search by starting with the
             # golden ratio
-            return SolutionInput(**get_next_input(
-                inp_dict, limits["min"] + (
-                    (limits["min"] - limits["max"]) / (GOLDEN_RATIO - 1)
+            return SolutionInput(
+                **get_next_input(
+                    inp_dict,
+                    limits["min"]
+                    + ((limits["min"] - limits["max"]) / (GOLDEN_RATIO - 1)),
                 )
-            ))
+            )
         if phase == Phase.BISECTING:
             log.notice("bisecting on attempt {}", attempt)
             if limits["mid"] is None:
                 limits["mid"] = current_v_r
                 limits["mid_val"] = current_quality
-                return SolutionInput(**get_next_input(
-                    inp_dict, limits["min"] + (limits["max"] - limits["mid"])
-                ))
+                return SolutionInput(
+                    **get_next_input(
+                        inp_dict, limits["min"] + (limits["max"] - limits["mid"])
+                    )
+                )
             if limits["mid"] > current_v_r:
                 lmid_lim = current_v_r
                 hmid_lim = limits["mid"]
@@ -153,9 +161,7 @@ def stepper_creator(soln_writer, *, step_size, solution_input):
                 limits["min_val"] = lmid_val
                 limits["mid"] = hmid_lim
                 limits["mid_val"] = hmid_val
-            new_v_r = limits["min"] + (
-                limits["max"] - limits["mid"]
-            )
+            new_v_r = limits["min"] + (limits["max"] - limits["mid"])
             if new_v_r == current_v_r:
                 StepperStop("Hit numerical limit")
             return SolutionInput(**get_next_input(inp_dict, new_v_r))
@@ -166,8 +172,11 @@ def stepper_creator(soln_writer, *, step_size, solution_input):
 
 
 def solution_generator(
-    *, store_internal=True, run, final_stop=DEFAULT_FINAL_STOP,
-    final_steps=DEFAULT_FINAL_STEPS
+    *,
+    store_internal=True,
+    run,
+    final_stop=DEFAULT_FINAL_STOP,
+    final_steps=DEFAULT_FINAL_STEPS,
 ):
     """
     Generate solution func
@@ -181,9 +190,7 @@ def solution_generator(
         solution func
         """
         try:
-            step_solver(
-                inp, run, store_internal=store_internal, no_final_solution=True
-            )
+            step_solver(inp, run, store_internal=store_internal, no_final_solution=True)
         except SolverError as e:
             log.info(e)
             invalid_soln = get_known_broken_solution(inp)
@@ -193,8 +200,10 @@ def solution_generator(
             stop=final_stop, num_angles=final_steps,
         )
         single_solver(
-            SolutionInput(**final_solution_dict), run,
-            store_internal=store_internal, no_final_solution=True,
+            SolutionInput(**final_solution_dict),
+            run,
+            store_internal=store_internal,
+            no_final_solution=True,
         )
         return get_last_solution(), False
 
@@ -202,8 +211,13 @@ def solution_generator(
 
 
 def solver(
-    soln_input, run, *, store_internal=True, final_stop=DEFAULT_FINAL_STOP,
-    final_steps=DEFAULT_FINAL_STEPS, num_attempts=DEFAULT_NUM_ATTEMPTS,
+    soln_input,
+    run,
+    *,
+    store_internal=True,
+    final_stop=DEFAULT_FINAL_STOP,
+    final_steps=DEFAULT_FINAL_STEPS,
+    num_attempts=DEFAULT_NUM_ATTEMPTS,
 ):
     """
     fast-crosser solver
@@ -211,20 +225,21 @@ def solver(
     uses step solver and single solver to cross fast speed
     """
     writer = writer_generator(run)
-    cleanup = cleanup_generator(
-        run, writer, no_final_solution=True,
-    )
+    cleanup = cleanup_generator(run, writer, no_final_solution=True,)
     best_solution = binary_searcher(
         solution_generator(
-            store_internal=store_internal, run=run, final_stop=final_stop,
+            store_internal=store_internal,
+            run=run,
+            final_stop=final_stop,
             final_steps=final_steps,
-        ), cleanup,
+        ),
+        cleanup,
         stepper_creator(
-            writer, step_size=0.01 * soln_input.v_rin_on_c_s,
-            solution_input=soln_input
+            writer, step_size=0.01 * soln_input.v_rin_on_c_s, solution_input=soln_input
         ),
         alternate_cleanup_generator(run, no_final_solution=True),
-        soln_input, num_attempts=num_attempts,
+        soln_input,
+        num_attempts=num_attempts,
     )
     if not isinstance(run.final_solution, Solution):
         if isinstance(best_solution, Solution):
