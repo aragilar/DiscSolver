2bf919554f0e36849954b89459fcd4a127be7409
James Tocknell <aragilar@gmail.com>
2022-06-10 13:07:42 +1000

    Add compute_jacobian

--- tests/test_run.py
+++ tests/test_run.py
@@ -7,6 +7,7 @@ from disc_solver.analyse.acc_plot import acc_plot
 from disc_solver.analyse.combine_plot import combine_plot
 from disc_solver.analyse.compare_plot import compare_plot
 from disc_solver.analyse.component_plot import plot as component_plot
+from disc_solver.analyse.compute_jacobian import compute_jacobian_from_file
 from disc_solver.analyse.conserve_plot import conserve_main
 from disc_solver.analyse.derivs_plot import derivs_plot
 from disc_solver.analyse.diverge_plot import diverge_main
@@ -518,6 +519,9 @@ class TestAnalysis:
 
     def test_stats(self, solution, tmpdir):
         return stats_main([solution, '--file', str(tmpdir / 'stats.csv')])
+
+    def test_compute_jacobian_from_solution(self, solution):
+        compute_jacobian_from_file(solution, eps=1e-10)
 
 
 def test_taylor_space(mpl_interactive):
