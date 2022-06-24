0d15fd4a7f6276455f1b39eddbde81844495f38e
James Tocknell <aragilar@gmail.com>
2022-06-24 15:53:51 +1000

    WIP

--- setup.py
+++ setup.py
@@ -57,6 +57,8 @@ setuptools.setup(
             "ds-params-plot = disc_solver.analyse.params_plot:params_main",
             "ds-taylor-plot = disc_solver.analyse.taylor_plot:taylor_main",
             "ds-combine-plot = disc_solver.analyse.combine_plot:combine_main",
+            "ds-jacobian-plot = disc_solver.analyse.jacobian_plot:jacobian_main",
+            "ds-jacobian-eigenvalue-plot = disc_solver.analyse.compute_jacobian:jacobian_eigenvalues_main",
             "ds-validate-plot = disc_solver.analyse.validate_plot:validate_plot_main",
             "ds-hydro-check-plot = disc_solver.analyse.hydro_check_plot:hydro_check_plot_main",
             "ds-acc-plot = disc_solver.analyse.acc_plot:acc_main",
--- src/disc_solver/analyse/compute_jacobian.py
+++ src/disc_solver/analyse/compute_jacobian.py
@@ -1,15 +1,59 @@
 # -*- coding: utf-8 -*-
 """
 """
+from functools import wraps
 
+import numpy as np
+from numpy import degrees
 from numpy import zeros
+from scipy.linalg import eigvals
 
 from ..float_handling import float_type
 from ..solve.solution import ode_system
 from ..utils import get_solutions
+from ..utils import ODEIndex
 from .utils import (
-    analyse_main_wrapper, analysis_func_wrapper,
+    single_solution_plotter, common_plotting_options, analyse_main_wrapper,
+    get_common_plot_args, analysis_func_wrapper, plot_output_wrapper,
+    DEFAULT_MPL_STYLE,
 )
+
+
+def plot_parser(parser):
+    """
+    Add arguments for plot command to parser
+    """
+    common_plotting_options(parser)
+    parser.add_argument(
+        "--eps", action='store', default=1e-10, type=float
+    )
+    return parser
+
+
+def get_plot_args(args):
+    """
+    Parse plot args
+    """
+    return {
+        "eps": float(args.get("eps", 1e-10)),
+    }
+
+
+@analyse_main_wrapper(
+    "Plot eigenvalues of jacobians for DiscSolver",
+    plot_parser,
+    cmd_parser_splitters={
+        "common_plot_args": get_common_plot_args,
+        "plot_args": get_plot_args,
+    }
+)
+def jacobian_eigenvalues_main(soln, *, soln_range, common_plot_args, plot_args):
+    """
+    Entry point for ds-derivs-plot
+    """
+    return jacobian_eigenvalues_plot(
+        soln, soln_range=soln_range, **common_plot_args, **plot_args
+    )
 
 
 def compute_jacobian(
@@ -76,3 +120,82 @@ def compute_jacobian_from_file(
 
     soln = get_solutions(soln_file, soln_range)
     return compute_jacobian_from_solution(soln, eps=eps, θ_scale=θ_scale)
+
+
+def jacobian_single_solution_plot_wrapper(func):
+    @wraps(func)
+    def jacobian_wrapper(
+        fig, soln, *args, eps, θ_scale=float_type(1), **kwargs
+    ):
+        jacobians = compute_jacobian_from_solution(
+            soln, eps=eps, θ_scale=θ_scale
+        )
+        return func(
+            fig, *args, jacobians=jacobians, angles=soln.angles, **kwargs
+        )
+
+    return jacobian_wrapper
+
+
+@analysis_func_wrapper
+def jacobian_eigenvalues_plot(
+    soln, *, soln_range=None, plot_filename=None, show=False, start=0, stop=90,
+    figargs=None, linestyle='.', title=None, close=True, filename,
+    mpl_style=DEFAULT_MPL_STYLE, with_version=True, eps
+):
+    """
+    Show derivatives
+    """
+    # pylint: disable=too-many-function-args,unexpected-keyword-arg
+    fig = plot_jacobian_eigenvalues(
+        soln, soln_range, linestyle=linestyle, start=start, stop=stop,
+        figargs=figargs, title=title, filename=filename,
+        mpl_style=mpl_style, with_version=with_version, eps=eps
+    )
+
+    return plot_output_wrapper(
+        fig, file=plot_filename, show=show, close=close
+    )
+
+
+def compute_eigenvalues(jacobian):
+    if not np.isfinite(jacobian).all():
+        return np.full(jacobian.shape[0], np.nan)
+    return eigvals(jacobian)
+
+
+@single_solution_plotter
+@jacobian_single_solution_plot_wrapper
+def plot_jacobian_eigenvalues(
+    fig, *, jacobians, angles, use_E_r, linestyle='.', figargs=None, start=0,
+    stop=90
+):
+    """
+    Generate plot of jacobians
+    """
+    if figargs is None:
+        figargs = {}
+
+    npnot = np.logical_not
+    npand = np.logical_and
+    indexes = degrees(angles) <= stop
+
+    data = np.array([compute_eigenvalues(j) for j in jacobians]).real
+
+    axes = fig.subplots(nrows=2, ncols=6, sharex=True, **figargs)
+    axes.shape = 12
+    for param in ODEIndex:
+        ax = axes[param]
+        pos_data = data[:, param] >= 0
+        ax.plot(
+            degrees(angles[npand(pos_data, indexes)]),
+            data[npand(pos_data, indexes), param], linestyle + "b",
+        )
+        ax.plot(
+            degrees(angles[npand(npnot(pos_data), indexes)]),
+            - data[npand(npnot(pos_data), indexes), param], linestyle + "g",
+        )
+        ax.set_xlabel("angle from plane (°)")
+        ax.set_yscale("log")
+        ax.set_title(param.name)
+    return fig
