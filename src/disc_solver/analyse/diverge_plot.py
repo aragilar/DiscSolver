6cb24b80514e7ebb5b832d17199587d33a303417
James Tocknell <aragilar@gmail.com>
2024-07-30 15:43:39 +1000

    Add arguments to make diverge plot look nicer
    For thesis plots

--- src/disc_solver/analyse/diverge_plot.py
+++ src/disc_solver/analyse/diverge_plot.py
@@ -2,6 +2,7 @@
 """
 Diverge-plot command for DiscSolver
 """
+import logbook
 import numpy as np
 from numpy import degrees
 import matplotlib.pyplot as plt
@@ -15,6 +16,8 @@ from .utils import (
 from ..utils import (
     mhd_wave_speeds, MHD_Wave_Index, ODEIndex, MAGNETIC_INDEXES
 )
+
+logger = logbook.Logger(__name__)
 
 NUM_ITEMS_PER_LEGEND_COLUMN = 15
 SLOW_STYLE = '-.'
@@ -30,17 +33,42 @@ def diverge_options(parser):
     parser.add_argument("--with-slow", action='store_true', default=False)
     parser.add_argument("--with-alfven", action='store_true', default=False)
     parser.add_argument("--with-fast", action='store_true', default=False)
+    parser.add_argument("--solutions", action='store', default=None)
+    parser.add_argument(
+        "--hide-legend", action='store_false', default=True,
+        dest="show_legend",
+    )
     return parser
+
+
+def parse_solution_filter(filter_string):
+    """
+    Parse solution filter yielding solution strings.
+    """
+    for entry in filter_string.strip().split(','):
+        entry = entry.strip()
+        if '-' in entry:
+            start, stop = entry.split('-', 2)
+            for i in range(int(start), int(stop)):
+                yield str(i)
+            yield stop
+        else:
+            yield entry
 
 
 def get_plot_args(args):
     """
     Parse plot args
     """
+    solution_filter = args.get("solutions", None)
+    if solution_filter is not None:
+        solution_filter = list(parse_solution_filter(solution_filter))
     return {
         "with_slow": args.get("with_slow", False),
         "with_alfven": args.get("with_alfven", False),
         "with_fast": args.get("with_fast", False),
+        "show_legend": args.get("show_legend", True),
+        "solution_filter": solution_filter,
     }
 
 
@@ -63,14 +91,14 @@ def diverge_main(*, solutions, common_plot_args, plot_args, filename):
 
 def generate_diverge_plot(
     solutions, *, figargs=None, start=0, stop=90, linestyle='-',
-    with_slow=False, with_alfven=False, with_fast=False,
+    with_slow=False, with_alfven=False, with_fast=False, show_legend=True,
 ):
     """
     Generate plot to compare how different runs change in v_θ
     """
     if figargs is None:
         figargs = {}
-    fig, ax = plt.subplots(**figargs)
+    fig, ax = plt.subplots(constrained_layout=True, **figargs)
 
     colors = distinct_color_map(len(solutions))
 
@@ -116,7 +144,8 @@ def generate_diverge_plot(
 
     ax.set_xlabel("$θ$ — angle from plane (°)")
     ax.set_ylabel("$v_θ / c_s$")
-    ax.legend(ncol=max(1, num_lines//NUM_ITEMS_PER_LEGEND_COLUMN))
+    if show_legend:
+        ax.legend(ncol=max(1, num_lines//NUM_ITEMS_PER_LEGEND_COLUMN))
     add_label_display_on_select(ax)
     return fig
 
@@ -125,20 +154,25 @@ def diverge_plot(
     solutions, *, plot_filename=None, show=False, start=0, stop=90,
     figargs=None, title=None, linestyle='-', with_slow=False, close=True,
     mpl_style=DEFAULT_MPL_STYLE, with_version=True, with_alfven=False,
-    with_fast=False, filename
+    with_fast=False, filename, show_legend=True, solution_filter=None,
 ):
     """
     Plot solution to file, with velocities, fields onto on one plot
     """
+    if solution_filter is not None:
+        soln_map = dict(solutions)
+        solutions = [
+            (soln_id, soln_map[soln_id]) for soln_id in solution_filter
+        ]
     with use_style(mpl_style):
         fig = generate_diverge_plot(
             solutions, start=start, stop=stop, figargs=figargs,
             linestyle=linestyle, with_slow=with_slow, with_alfven=with_alfven,
-            with_fast=with_fast,
+            with_fast=with_fast, show_legend=show_legend,
         )
         if title is None:
             fig.suptitle(f"Comparison of v_θ for {filename}")
-        else:
+        elif title:
             fig.suptitle(title)
 
     if with_version:
